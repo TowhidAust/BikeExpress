9bfe8531ecef920de1a3b8586bd549c0037521ef
TowhidAust <007towhid2016@gmail.com>
2021-08-27 17:10:57 +0600

    folder restructured and converted into npm

--- components/index.js
+++ components/index.js
@@ -1,10 +1,10 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+// import styles from '../styles/Home.module.css'
 import SlickCarousel from './common/slickCarousel'
 
 export default function Index() {
   return (
-    <div className="container">
+    <div style={{width:"95vw"}}>
       <SlickCarousel/>
     </div>
   )
