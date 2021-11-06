25679d8b6963a35db8b202d9f3df2225be89d1b6
TowhidAust <007towhid2016@gmail.com>
2021-11-06 22:53:18 +0600

    dynamic one stop card fix

--- components/index.js
+++ components/index.js
@@ -4,7 +4,20 @@ import BudgetBikes from './budgetBikes';
 import Banner from './common/banner/banner';
 import SlickCarousel from './common/slickCarousel/slick';
 import OneStopCard from './oneStopCard';
+import TwoWheelerIcon from '@mui/icons-material/TwoWheeler';
 export default function Index() {
+
+  const returnIcon = (iconName) => {
+   if(iconName==="usedBike") {
+     return  <TwoWheelerIcon sx={{ color: "red", fontSize: "5em" }} /> 
+   }
+   if(iconName==="sellBike") {
+    return  <TwoWheelerIcon sx={{ color: "red", fontSize: "5em" }} /> 
+  }
+  if(iconName==="exchangeBike") {
+    return  <TwoWheelerIcon sx={{ color: "red", fontSize: "5em" }} /> 
+  }
+  }
   return (
     <>
       <Banner />
@@ -14,13 +27,13 @@ export default function Index() {
           <Box sx={{ flexGrow: 1 }}>
             <Grid container spacing={2}>
               <Grid item xs={12} md={4}>
-                <OneStopCard />
+                <OneStopCard title="BUY USED BIKES" description="Buy the best quality used bikes from BikeHub at a reasonable price." icon={returnIcon('usedBike')} />
               </Grid>
               <Grid item xs={12} md={4}>
-                <OneStopCard />
+                <OneStopCard title="SELL YOUR BIKE" description="Sell your bike to BikeHub at the best price with instant payment." icon={returnIcon('sellBike')} />
               </Grid>
               <Grid item xs={12} md={4}>
-                <OneStopCard />
+                <OneStopCard title="EXCHANGE YOUR BIKE" description="Bring your old bike and get it exchanged with a new bike at the best price." icon={returnIcon('exchangeBike')} />
               </Grid>
             </Grid>
           </Box>
--- pages/_app.js
+++ pages/_app.js
@@ -9,7 +9,7 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <>
           <title>BikeHub</title>
-          <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
+          <link rel="stylesheet" type="text/css" charSet="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
           <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
         </>
       </Head>
