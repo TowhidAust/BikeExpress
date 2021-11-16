7257ca24d642a825ca77f9815135fcc37bfb0ac2
TowhidAust <007towhid2016@gmail.com>
2021-11-16 23:50:24 +0600

    bike shop card design updates

--- components/all-used-bikes/shop/index.js
+++ components/all-used-bikes/shop/index.js
@@ -1,6 +1,6 @@
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
-import { CardActionArea } from '@mui/material';
+import { Avatar, CardActionArea, CardMedia, Typography } from '@mui/material';
 import { Grid } from "@mui/material"
 import { Box } from "@mui/system"
 
@@ -8,9 +8,10 @@ const BikeShops = (props) => {
     const returnAllShops = () => {
         let shopArr = [
             {
-                shopName: "BikeHub",
+                shopName: "BIKEHUB",
+                logo: "https://i.pinimg.com/originals/2e/d1/15/2ed115c13891fd913afe5d2f32dfa85f.jpg",
                 ownerName: "Md. Towhidul Islam",
-                shopType:"bike/accessories",
+                shopType: "bike/accessories",
                 location: "Boshundhora R/A",
                 nationalId: "19938506000010",
                 email: "007towhid2016@gmail.com",
@@ -20,8 +21,33 @@ const BikeShops = (props) => {
             },
             {
                 shopName: "SAWARI",
+                logo: "https://cdn.dribbble.com/users/4553005/screenshots/14091351/motor_bike_logo-01_4x.png",
                 ownerName: "Md. Towhidul Islam",
-                shopType:"bike/accessories",
+                shopType: "bike/accessories",
+                location: "Boshundhora R/A",
+                nationalId: "19938506000010",
+                email: "007towhid2016@gmail.com",
+                password: "123456",
+                tin: "1234",
+                tradeLicence: "78932"
+            },
+            {
+                shopName: "NOORJAHAN MOTORS",
+                logo: "https://images.creativemarket.com/0.1.0/ps/5672096/2627/1753/m1/fpnw/wm1/biker-.jpg?1547024729&s=f853cd91429153b8c61bbfc3aa9150a1",
+                ownerName: "Md. Towhidul Islam",
+                shopType: "bike/accessories",
+                location: "Boshundhora R/A",
+                nationalId: "19938506000010",
+                email: "007towhid2016@gmail.com",
+                password: "123456",
+                tin: "1234",
+                tradeLicence: "78932"
+            },
+            {
+                shopName: "MOTOEXPRESS BD",
+                logo: "https://cdn.dribbble.com/users/4553005/screenshots/14091351/motor_bike_logo-01_4x.png",
+                ownerName: "Md. Towhidul Islam",
+                shopType: "bike/accessories",
                 location: "Boshundhora R/A",
                 nationalId: "19938506000010",
                 email: "007towhid2016@gmail.com",
@@ -33,31 +59,37 @@ const BikeShops = (props) => {
         console.log(shopArr)
         return shopArr.map((item, index) => {
             return (
-                <Grid key={index} xs={12} md={6} className="p-2">
-                    <Card>
-                        <CardActionArea>
-                            <CardContent>
-                            <Grid container spacing={2}>
-                                <Grid xs={12} md={2} className="p-2">
-                                     <img className="image-width-100 shop-imgae" src="/images/default-bike.jpg"/>
-                                </Grid>
-                                <Grid xs={12} md={10} className="p-2">
-                                    <div className="title">{item.shopName}</div>
-                                    <div>{item.email}</div>
-                                    <div>{item.shopType}</div>
-                                    <div>{item.location}</div>
-                                </Grid>
-                            </Grid>
-                            </CardContent>
-                        </CardActionArea>
-                    </Card>
+                <Grid key={index} xs={12} md={4} className="p-2">
+                    <CardActionArea>
+                        <Card sx={{ display: 'flex' }}>
+                            <CardMedia
+                                component="img"
+                                sx={{ width: 151 }}
+                                image={item.logo}
+                                alt="Live from space album cover"
+                            />
+                            <Box sx={{ display: 'flex', flexDirection: 'column', width: "100%" }}>
+                                <CardContent sx={{ flex: '1 0 auto' }}>
+                                    <Typography component="div">
+                                        {item.shopName}
+                                    </Typography>
+                                    <Typography variant="subtitle2" color="text.secondary" component="div">
+                                        {item.location}
+                                    </Typography>
+                                    <Typography variant="subtitle2" color="text.secondary" component="div">
+                                        {item.shopType}
+                                    </Typography>
+                                </CardContent>
+                            </Box>
+                        </Card>
+                    </CardActionArea>
                 </Grid>
             )
         })
     }
     return (
         <div className="all-bike-shop-root mt-1">
-            <div className="all-shops container">
+            <div className="all-shops">
                 <Grid container>
                     {returnAllShops()}
                 </Grid>
