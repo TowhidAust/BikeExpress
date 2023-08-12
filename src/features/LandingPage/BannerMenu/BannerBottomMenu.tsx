bb6cf51e0d0055e7a9500185be18a375e4ed94e0
TowhidAust <007towhid2016@gmail.com>
2023-08-12 11:02:20 +0600

    Spare parts section added in the top banner

--- src/features/LandingPage/BannerMenu/BannerBottomMenu.tsx
+++ src/features/LandingPage/BannerMenu/BannerBottomMenu.tsx
@@ -1,6 +1,6 @@
 import { RiOilLine, RiToolsLine } from 'react-icons/ri';
 import { GiCartwheel, GiLoveHowl, GiSchoolBag, GiSmartphone, GiStickGrenade } from 'react-icons/gi';
-import { Button, Card, Col, Row } from 'antd';
+import { Card, Col, Divider, Row, Typography } from 'antd';
 import { FaGripHorizontal } from 'react-icons/fa';
 import { AiOutlineCamera, AiOutlinePhone } from 'react-icons/ai';
 
@@ -8,77 +8,118 @@ export default function BannerBottomMenu() {
 	const bannerBottomMenuCategories = [
 		{
 			key: 1,
-			categoryName: 'Engine Oil',
+			categoryName: 'SUZUKI',
 			categoryIcon: RiOilLine,
 		},
 		{
 			key: 2,
-			categoryName: 'Spare Parts',
+			categoryName: 'YAMAHA',
 			categoryIcon: RiToolsLine,
 		},
 		{
 			key: 3,
-			categoryName: 'Bike Care',
+			categoryName: 'HONDA',
 			categoryIcon: GiLoveHowl,
 		},
 		{
 			key: 4,
-			categoryName: 'Bag Pack',
+			categoryName: 'TVS',
 			categoryIcon: GiSchoolBag,
 		},
 		{
 			key: 5,
-			categoryName: 'Bumper',
+			categoryName: 'BAJAJ',
 			categoryIcon: GiStickGrenade,
 		},
 		{
 			key: 6,
-			categoryName: 'Mobile Holder',
+			categoryName: 'HERO',
 			categoryIcon: GiSmartphone,
 		},
 		{
 			key: 7,
-			categoryName: 'Tire Gel',
+			categoryName: 'GPX',
 			categoryIcon: GiCartwheel,
 		},
 		{
 			key: 8,
-			categoryName: 'Grip',
+			categoryName: 'LIFAN',
 			categoryIcon: FaGripHorizontal,
 		},
 		{
 			key: 9,
-			categoryName: 'Camera',
+			categoryName: 'ZONTES',
 			categoryIcon: AiOutlineCamera,
 		},
 		{
 			key: 10,
-			categoryName: 'Communicator',
+			categoryName: 'KTM',
+			categoryIcon: AiOutlinePhone,
+		},
+		{
+			key: 11,
+			categoryName: 'APRILIA',
+			categoryIcon: AiOutlinePhone,
+		},
+		{
+			key: 12,
+			categoryName: 'KAWASAKI',
+			categoryIcon: AiOutlinePhone,
+		},
+		{
+			key: 13,
+			categoryName: 'BENELLI',
+			categoryIcon: AiOutlinePhone,
+		},
+		{
+			key: 14,
+			categoryName: 'KEEWAY',
+			categoryIcon: AiOutlinePhone,
+		},
+		{
+			key: 15,
+			categoryName: 'TARO',
+			categoryIcon: AiOutlinePhone,
+		},
+		{
+			key: 16,
+			categoryName: 'VESPA',
+			categoryIcon: AiOutlinePhone,
+		},
+		{
+			key: 17,
+			categoryName: 'ROADMASTER',
+			categoryIcon: AiOutlinePhone,
+		},
+		{
+			key: 18,
+			categoryName: 'SPEEDER',
+			categoryIcon: AiOutlinePhone,
+		},
+		{
+			key: 19,
+			categoryName: 'HPOWER',
 			categoryIcon: AiOutlinePhone,
 		},
 	];
 	return (
-		<Row gutter={[16, 16]} className="mt-3">
+		<Row gutter={[6, 6]} className="mt-1">
+			<Col span={24}>
+				<Typography.Title className="m-0" level={4}>
+					Spare Parts
+				</Typography.Title>
+				<Divider className="mt-2 mb-2" />
+			</Col>
 			{bannerBottomMenuCategories?.map((item) => (
 				<Col xs={24} sm={24} md={4} key={Math.random()}>
 					<Card
 						hoverable
-						className="p-0 text-center"
+						className="p-0 text-center primary-font-color"
 						bodyStyle={{
-							padding: '0px',
+							padding: '10px',
 						}}
 					>
-						<Button
-							block
-							style={{
-								fontSize: 14,
-								textAlign: 'center',
-							}}
-							type="link"
-							size="large"
-						>
-							{item?.categoryName}
-						</Button>
+						{item?.categoryName}
 					</Card>
 				</Col>
 			))}
